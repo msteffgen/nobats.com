bd40e88d203639bc367abc11302130934d1ce60d
Mike Steffgen <mike.steffgen@wavereaction.com>
2026-01-29 16:11:14 -0600

    Add Google Analytics tag to each page

---
+++ js/analytics.js
@@ -0,0 +1,11 @@
+// Load the external Google Analytics script
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-DY1MP35ETY';
+document.head.appendChild(script);
+
+// Initialize the tracking
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-XXXXXXXXXX');
